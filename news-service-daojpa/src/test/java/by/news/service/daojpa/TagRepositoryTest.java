a74f42d9a2a491022d958a9332f38b1e776f79e2
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-09-14 19:40:32 +0300

    try spring integration

---
+++ news-service-daojpa/src/test/java/by/news/service/daojpa/TagRepositoryTest.java
@@ -0,0 +1,37 @@
+package by.news.service.daojpa;
+
+import by.news.service.daojpa.pojos.News;
+import by.news.service.daojpa.repository.interf.TagRepository;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import java.util.Set;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class})
+@DatabaseSetup("classpath:TagRepoTest.xml")
+public class TagRepositoryTest {
+
+    private Logger Log = LogManager.getLogger(UserRepositoryTest.class.getName());
+
+    @Autowired
+    TagRepository tagRepository;
+
+    @Test
+    public void testGetNewsByTag() {
+        Set<News> newses = tagRepository.findNewses("Tag1");
+        Assert.assertNotNull(newses);
+    }
+}
+
